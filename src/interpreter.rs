9e113e4fbb93a8eb20dd4100450b7e37afc4f53d
Brayden Chan <72172597+megabyte6@users.noreply.github.com>
2024-06-05 19:56:59 -0700

    Add support for optimized insruction counting

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -6,33 +6,43 @@ use crate::preprocessor::Instruction;
 pub fn execute(instructions: &Vec<Instruction>, tape: &mut Vec<u8>, pointer: &mut usize) {
     for instruction in instructions {
         match instruction {
-            Instruction::IncrementPointer => {
-                *pointer += 1;
-                if *pointer == tape.len() {
-                    *pointer = 0;
-                }
-            }
-            Instruction::DecrementPointer => {
-                if *pointer == 0 {
-                    *pointer = tape.len();
-                }
-                *pointer -= 1;
-            }
-            Instruction::IncrementValue => {
-                if tape[*pointer] == 255 {
-                    tape[*pointer] = 0;
+            Instruction::MoveRight(count) => {
+                let increment = *count % tape.len();
+                let (new_pointer, overflow) = pointer.overflowing_add(increment);
+                *pointer = if overflow {
+                    new_pointer + (usize::MAX - tape.len() + 1)
+                } else if new_pointer >= tape.len() {
+                    new_pointer - tape.len()
                 } else {
-                    tape[*pointer] += 1;
-                }
+                    new_pointer
+                };
             }
-            Instruction::DecrementValue => {
-                if tape[*pointer] == 0 {
-                    tape[*pointer] = 255;
+            Instruction::MoveLeft(count) => {
+                let decrement = *count % tape.len();
+                let (new_pointer, overflow) = pointer.overflowing_sub(decrement);
+                *pointer = if overflow {
+                    tape.len() - (decrement - *pointer)
                 } else {
-                    tape[*pointer] -= 1;
+                    new_pointer
+                };
+            }
+            Instruction::Increment(count) => {
+                tape[*pointer] =
+                    tape[*pointer].wrapping_add((*count % (u8::MAX as usize + 1)) as u8)
+            }
+
+            Instruction::Decrement(count) => {
+                tape[*pointer] =
+                    tape[*pointer].wrapping_sub((*count % (u8::MAX as usize + 1)) as u8);
+            }
+            Instruction::Write(count) => {
+                if *count == 1 {
+                    print!("{}", tape[*pointer] as char);
+                } else {
+                    let data = vec![tape[*pointer]; *count];
+                    print!("{}", String::from_utf8(data).unwrap());
                 }
             }
-            Instruction::Write => print!("{}", tape[*pointer] as char),
             Instruction::Read => {
                 let mut input: [u8; 1] = [0];
                 stdin()
