60be4447b61a2ae03ff8970e8cf77fab4bc027ab
Brayden Chan <72172597+megabyte6@users.noreply.github.com>
2025-01-12 13:32:10 -0800

    Improve error messages and enhance code readability in interpreter and preprocessor

--- src/preprocessor.rs
+++ src/preprocessor.rs
@@ -68,12 +68,9 @@ pub fn lex(source: &str) -> Result<Vec<Token>, Vec<SyntaxError>> {
     }
 
     if !open_loops.is_empty() {
-        for location in open_loops {
-            errors.push(SyntaxError::from_source_location(
-                &location,
-                Box::new(LoopError::MissingEnd),
-            ));
-        }
+        errors.extend(open_loops.iter().map(|location| {
+            SyntaxError::from_source_location(location, Box::new(LoopError::MissingEnd))
+        }));
     }
 
     if errors.is_empty() {
@@ -110,15 +107,9 @@ pub fn parse(tokens: Vec<Token>) -> Result<Vec<Instruction>, SyntaxError> {
             Token::Write(_) => Instruction::Write(count),
             Token::Read(_) => Instruction::Read,
             Token::LoopStart(source_location) => {
-                let end_index = match end_loop_index(&tokens, index) {
-                    Ok(index) => index,
-                    Err(error) => {
-                        return Err(SyntaxError::from_source_location(
-                            source_location,
-                            Box::new(error),
-                        ))
-                    }
-                };
+                let end_index = end_loop_index(&tokens, index).map_err(|error| {
+                    SyntaxError::from_source_location(source_location, Box::new(error))
+                })?;
                 let loop_content = tokens[index + 1..end_index].to_vec();
                 // Skip to the end of the loop
                 count = end_index - index + 1;
